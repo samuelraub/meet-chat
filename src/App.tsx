b106cb4e43cdbfe65502c17d1d5844a9df2c62d7
samuel.raub@livingpackets.com <samuelraub@Samuels-MBP.fritz.box>
2021-07-31 18:03:09 +0200

    works

--- src/App.tsx
+++ src/App.tsx
@@ -1,81 +1,131 @@
 import * as React from "react";
-import { ChangeEvent, useState } from "react";
+import { ChangeEvent, useRef, useState } from "react";
 import {
   Box,
   Button,
   ChakraProvider,
+  Divider,
   Grid,
+  Heading,
+  Input,
   Textarea,
   theme,
-  Heading,
 } from "@chakra-ui/react";
+import { CopyIcon, CheckIcon } from "@chakra-ui/icons";
 import ReactMarkdown from "react-markdown";
-import CopyToClipboard from "react-copy-to-clipboard";
 import gfm from "remark-gfm";
+import CopyToClipboard from "react-copy-to-clipboard";
 
-const style = {
-  border: "1px solid black",
-  borderRadius: "0.5rem",
-  margin: "1rem",
-  background: "white",
+const doSomethingToNameLines = (str: string, cb: (str: string) => string) => {
+  const arr = str.split(/\r?\n/);
+  return arr.map((line) => {
+    if (/\d*:\d\d/.test(line)) {
+      return cb(line);
+    }
+    return line;
+  });
 };
 
 export const App = () => {
   const [output, setOutput] = useState("");
 
+  const [searchReplace, setSearchReplace] = useState(["", ""]);
+
   const handleChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
     const val = e?.target?.value;
-    console.log(val);
-    const arr = val.split(/\r?\n/);
-    const outString = arr
-      .map((line) => {
-        if (/\d*:\d\d/.test(line)) {
-          const index = line.match(/\d/)?.index;
-          const modLine = line.slice(0, index) + " " + line.slice(index);
-          return `# ${modLine}`;
-        }
-        return line;
-      })
-      .join("\n\n");
+    const outString = doSomethingToNameLines(val, (line) => {
+      const index = line.match(/\d/)?.index;
+      const modLine = line.slice(0, index) + " " + line.slice(index);
+      return `# ${modLine}`;
+    }).join("\n\n");
     setOutput(outString);
   };
 
+  const handleClick = () => {
+    const range = document.createRange();
+    range.selectNode(resultBox.current);
+    window.getSelection()?.removeAllRanges();
+    window.getSelection()?.addRange(range);
+    document.execCommand("copy");
+    window.getSelection()?.removeAllRanges();
+  };
+
+  const resultBox = useRef<any>(null);
+
   return (
     <ChakraProvider theme={theme}>
-      <Box textAlign="left" fontSize="xl" background={"lightcoral"}>
-        <Grid minH="100vh" padding={"5rem"} templateColumns={"1fr 1fr"}>
-          <Box style={style}>
+      <Box textAlign="left" fontSize="xl" background={"white"}>
+        <Heading textAlign={"center"}>🤝 Meet Chat Formatter</Heading>
+        <Grid padding={"5rem"} templateColumns={"1fr 1fr"} gap={"1rem"}>
+          <Box>
             <Textarea
-              variant={"unstyled"}
+              placeholder={"Paste Google Meet Chat"}
+              variant={"filled"}
               onChange={handleChange}
               height={"100%"}
               resize={"none"}
               padding={"1rem"}
             />
           </Box>
-          <Box style={{ ...style, padding: "0 2rem", position: "relative" }}>
-            <ReactMarkdown
-              components={{
-                h1: ({ children }) => (
-                  <Heading marginTop={"1rem"} size={"md"}>
-                    {children}
-                  </Heading>
-                ),
-              }}
-              remarkPlugins={[gfm]}
-            >
-              {output}
-            </ReactMarkdown>
-            <CopyToClipboard text={output}>
-              <Button
-                margin={"0.5rem"}
-                position={"absolute"}
-                bottom={"0"}
-                right={"0"}
+          <Box padding={"1rem 2rem"} background={"gray.100"}>
+            <div ref={resultBox}>
+              <ReactMarkdown
+                components={{
+                  h1: ({ children }) => (
+                    <Heading marginTop={"1rem"} size={"md"}>
+                      {children}
+                    </Heading>
+                  ),
+                }}
+                remarkPlugins={[gfm]}
               >
-                Copy
+                {output}
+              </ReactMarkdown>
+            </div>
+          </Box>
+          <Box></Box>
+          <Box>
+            <Button leftIcon={<CopyIcon />} onClick={handleClick}>
+              Copy formatted
+            </Button>
+            <CopyToClipboard text={output}>
+              <Button leftIcon={<CopyIcon />} marginLeft={"1rem"}>
+                Copy markdown
               </Button>
             </CopyToClipboard>
+          </Box>
+        </Grid>
+        <Divider />
+        <Grid padding={"2rem 5rem"} templateColumns={"1fr 1fr"} gap={"1rem"}>
+          <Box>
+            <Input
+              variant={"filled"}
+              placeholder={"Search Name"}
+              onChange={(e) =>
+                setSearchReplace((prev) => [e.target.value, prev[1]])
+              }
+            />
+          </Box>
+          <Box>
+            <Input
+              variant={"filled"}
+              placeholder={"Replace With"}
+              onChange={(e) =>
+                setSearchReplace((prev) => [prev[0], e.target.value])
+              }
+            />
+            <Button
+              marginTop={"1rem"}
+              leftIcon={<CheckIcon />}
+              onClick={() => {
+                const outString = doSomethingToNameLines(output, (line) => {
+                  return line.replace(searchReplace[0], searchReplace[1]);
+                }).join("\n");
+                setOutput(outString);
+              }}
+            >
+              Confirm
+            </Button>
           </Box>
         </Grid>
       </Box>
